b43f27a0b116491728da36c27bd110d074b210c5
mohammedafeef <mohammedafeef.maf@gmail.com>
2022-07-25 16:12:16 +0530

    wop(joinform): configure base router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelmetProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </HelmetProvider>
 );
