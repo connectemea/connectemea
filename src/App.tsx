6eb5d129ff8b76713028df8370b3242a794f7768
mohammedafeef <mohammedafeef.maf@gmail.com>
2022-07-25 16:01:58 +0530

    wop(joinform): create base app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "./pages";
 function App() {
-  return <div>hello world</div>;
+  return (
+    <Routes>
+      <Route path="/">
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
