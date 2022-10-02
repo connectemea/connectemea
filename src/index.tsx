4d09dbba06b314d6b1e15f72b4d0e449dc876e01
mohammedafeef <mohammedafeef.maf@gmail.com>
2022-10-02 23:05:43 +0530

    fix: update addDelay utility delay time

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { HashRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <HelmetProvider>
     <Router basename="/">
